d57572903c721caeec9697f1c93c95f515f4605e
dpischalka <dpischalka@gmail.com>
2020-05-06 20:04:23 +0300

    Updates

---
+++ precache-manifest.259ea9426b6f11abd4262d7f3f9f6d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f81a71cc68d6240fa4971a1c4c429e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07f354e2c9bf142844a1",
+    "url": "/static/css/main.473f118f.chunk.css"
+  },
+  {
+    "revision": "a9a5214c85343f22edaa",
+    "url": "/static/js/2.bb76fb4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bb76fb4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f354e2c9bf142844a1",
+    "url": "/static/js/main.96034482.chunk.js"
+  },
+  {
+    "revision": "6bf8786463e2eefe1266",
+    "url": "/static/js/runtime-main.174d3796.js"
+  }
+]);
