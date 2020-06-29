fefe4a467a61e49645d6414f7c06f45655ccec5f
dpischalka <dpischalka@gmail.com>
2020-06-30 00:23:18 +0400

    Added "Media Links" placeholder.

--- src/components/content/conten.tsx
+++ src/components/content/conten.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 import './content.scss';
+import MediaLinksList from './media-links-list/media-links-list';
 
 function Content() {
   const authorName: string = 'Dmitry Pischalka';
   const description: string = 'Senior Front-End Developer, geek, blogger.';
 
   return (
-    <div className="content-part has-text-centered">
-      <h1 className="is-size-1 content-part-title">{authorName}</h1>
+    <main className="content-part">
+      <h1 className="is-size-1 content-part-title has-text-centered">{authorName}</h1>
 
-      <span>{description}</span>
-    </div>
+      <div className="content-description has-text-centered">{description}</div>
+      <MediaLinksList/>
+    </main>
   );
 }
 
---
+++ src/components/content/media-links-list/media-links-list.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './media-links-list.scss';
+
+function MediaLinksList() {
+  return (
+    <div className="media-links-container">test</div>
+  )
+}
+
+export default MediaLinksList;
