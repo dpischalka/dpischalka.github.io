ba431d46178c849d7400c17164821d5b75f0a8c3
dpischalka <dpischalka@gmail.com>
2020-07-03 23:51:31 +0400

    added dynamic icon size.

--- src/components/content/media-links-list/media-link-item/media-link-item.tsx
+++ src/components/content/media-links-list/media-link-item/media-link-item.tsx
@@ -4,10 +4,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './media-link-item.scss';
 import { MediaLinkItemProps } from "../media-links-list";
 
-function MediaLinkItem({className, title, icon, href}: MediaLinkItemProps) {
+function MediaLinkItem({className, title, icon, href, iconSize = '3x'}: MediaLinkItemProps) {
   return (
     <div className={className}>
-      <FontAwesomeIcon icon={icon} size="3x"/>
+      <FontAwesomeIcon icon={icon} size={iconSize}/>
       {
         href
           ? <a href={href} rel="noopener noreferrer" target="_blank">{title}</a>
