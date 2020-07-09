600057745d2b9850e813d5e95c3a42e72365ec2a
dpischalka <dpischalka@gmail.com>
2020-07-09 12:44:04 +0400

    Added temporary notification, edited "content.scss" styles.

--- src/containers/content/conten.tsx
+++ src/containers/content/conten.tsx
@@ -6,12 +6,14 @@ import MediaLinksList from './media-links-list/media-links-list';
 function Content() {
   const authorName: string = 'Dmitry Pischalka';
   const description: string = 'Senior Front-End Developer, geek, blogger.';
+  const temporaryNotification: string = 'Coming soon for blog, additional info, other...';
 
   return (
     <main className="content-part">
       <h1 className="is-size-1 content-part-title has-text-centered">{authorName}</h1>
 
       <div className="content-description has-text-centered">{description}</div>
+      <div className="content-temporary-notification has-text-centered">{temporaryNotification}</div>
       <MediaLinksList/>
     </main>
   );
