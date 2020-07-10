d04b88b2cec55306774404feea835a3f5fb76764
dpischalka <dpischalka@gmail.com>
2020-07-10 23:26:09 +0400

    Put out markup to 2 specific constants.

--- src/containers/content/media-links-list/media-link-item/media-link-item.tsx
+++ src/containers/content/media-links-list/media-link-item/media-link-item.tsx
@@ -6,26 +6,24 @@ import { MediaLinkItemProps } from "../media-links-list";
 
 function MediaLinkItem({className, title, icon, href, iconSize = '3x'}: MediaLinkItemProps) {
   const Icon: JSX.Element = <FontAwesomeIcon className="contact-item-icon" icon={icon} size={iconSize}/>;
-
-  return (
-    href
-      ? (
-        <a className={`contact-item contact-item-link ${className}`}
-           rel="noopener noreferrer"
-           target="_blank"
-           href={href}
-           title={href}>
-          <span className="contact-item-icon">{Icon}</span>
-          <span className="contact-item-title">{title}</span>
-        </a>
-      )
-      : (
-        <div className={`contact-item ${className}`} title={title}>
-          <span className="contact-item-icon">{Icon}</span>
-          <span className="contact-item-title">{title}</span>
-        </div>
-      )
+  const LinkEl: JSX.Element = (
+    <a className={`contact-item contact-item-link ${className}`}
+       rel="noopener noreferrer"
+       target="_blank"
+       href={href}
+       title={href}>
+      <span className="contact-item-icon">{Icon}</span>
+      <span className="contact-item-title">{title}</span>
+    </a>
   );
+  const SpanEl: JSX.Element = (
+    <div className={`contact-item ${className}`} title={title}>
+      <span className="contact-item-icon">{Icon}</span>
+      <span className="contact-item-title">{title}</span>
+    </div>
+  );
+
+  return (href ? LinkEl : SpanEl);
 }
 
 export default MediaLinkItem;
