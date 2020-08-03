7e5f16f30548c2f1695e61fe90d45ddb586ad8c4
dpischalka <dpischalka@gmail.com>
2020-08-03 23:18:50 +0400

    Improved logic for update icon size on windows resize.

--- src/containers/content/media-links-list/media-links-list.tsx
+++ src/containers/content/media-links-list/media-links-list.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import { faGithub, faLinkedinIn, faTwitter, faMedium } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { IconProp, SizeProp } from "@fortawesome/fontawesome-svg-core";
@@ -16,47 +16,54 @@ export interface MediaLinkItemProps {
   className?: string;
 }
 
+const ICON_SIZES: StringMap<SizeProp> = {
+  [SCREEN_SIZES_NAMES_LIST.TABLET]: '2x',
+  [SCREEN_SIZES_NAMES_LIST.DESKTOP]: '3x',
+  [SCREEN_SIZES_NAMES_LIST.FULLHD]: '3x',
+  [SCREEN_SIZES_NAMES_LIST.WIDESCREEN]: '3x',
+};
+
+const mediaLinksList: MediaLinkItemProps[] = [
+  {title: 'dpischalka@gmail.com', icon: faEnvelope, className: 'mail'},
+  {title: 'dpischalka', icon: faTwitter, href: 'https://twitter.com/dpischalka', className: 'twitter'},
+  {title: 'dpischalka', icon: faGithub, href: 'https://github.com/dpischalka', className: 'github'},
+  {title: 'dpischalka', icon: faLinkedinIn, href: 'https://www.linkedin.com/in/dpischalka/', className: 'linkedIn'},
+  {title: 'dpischalka', icon: faMedium, href: 'https://medium.com/@dpischalka/', className: 'medium'},
+];
+
+function iconSizeChangeHandler(mediaLinksState: MediaLinkItemProps[], mediaLinksStateSetter: Dispatch<SetStateAction<MediaLinkItemProps[]>>): void {
+  const SCREEN_WIDTH = detectScreenSize(window.innerWidth) ?? null;
+
+  if (SCREEN_WIDTH) {
+    mediaLinksStateSetter(mediaLinksList
+      .filter(({icon, title}: MediaLinkItemProps): boolean => Boolean(icon && title))
+      .map((mediaLinkItemProps: MediaLinkItemProps, index: number): MediaLinkItemProps => ({
+        ...mediaLinkItemProps,
+        className: `column media-link-item-${index} ${mediaLinkItemProps.className}`,
+        iconSize: ICON_SIZES[SCREEN_WIDTH],
+      })));
+  }
+}
+
 function MediaLinksList() {
-  const ICON_SIZES: StringMap<SizeProp> = {
-    [SCREEN_SIZES_NAMES_LIST.TABLET]: '1x',
-    [SCREEN_SIZES_NAMES_LIST.DESKTOP]: '3x',
-    [SCREEN_SIZES_NAMES_LIST.FULLHD]: '3x',
-    [SCREEN_SIZES_NAMES_LIST.WIDESCREEN]: '3x',
-  };
+  const [mediaItems, updateMediaItems] = useState<MediaLinkItemProps[]>(mediaLinksList);
 
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-      const SCREEN_WIDTH = detectScreenSize(window.innerWidth) ?? null;
-
-      if (SCREEN_WIDTH) {
-        mediaLinksList.map((mediaLinkItemProps: MediaLinkItemProps, index: number): JSX.Element => {
-          mediaLinkItemProps.iconSize = ICON_SIZES[SCREEN_WIDTH];
-
-          return <MediaLinkItem key={index} {...mediaLinkItemProps}/>
-        });
-      }
-      console.log(detectScreenSize(window.innerWidth));
-    });
-  });
-
-  const mediaLinksList: MediaLinkItemProps[] = [
-    {title: 'dpischalka@gmail.com', icon: faEnvelope, className: 'mail'},
-    {title: 'dpischalka', icon: faTwitter, href: 'https://twitter.com/dpischalka', className: 'twitter'},
-    {title: 'dpischalka', icon: faGithub, href: 'https://github.com/dpischalka', className: 'github'},
-    {title: 'dpischalka', icon: faLinkedinIn, href: 'https://www.linkedin.com/in/dpischalka/', className: 'linkedIn'},
-    {title: 'dpischalka', icon: faMedium, href: 'https://medium.com/@dpischalka/', className: 'medium'},
-  ];
-  const [mediaItems] = useState<MediaLinkItemProps[]>(mediaLinksList);
   const mediaLinkItems = mediaItems
     .filter(({icon, title}: MediaLinkItemProps): boolean => Boolean(icon && title))
-    .map((mediaLinkItemProps: MediaLinkItemProps, index: number): JSX.Element => {
-      mediaLinkItemProps.className = `column media-link-item-${index} ${mediaLinkItemProps.className}`;
+    .map((mediaLinkItemProps: MediaLinkItemProps, index: number): MediaLinkItemProps => ({
+      ...mediaLinkItemProps,
+      className: `column media-link-item-${index} ${mediaLinkItemProps.className}`,
+    }));
 
-      return <MediaLinkItem key={index} {...mediaLinkItemProps}/>
-    });
+  useEffect(() => {
+    window.addEventListener('resize', iconSizeChangeHandler.bind(MediaLinksList, mediaItems, updateMediaItems));
+    window.addEventListener('load', iconSizeChangeHandler.bind(MediaLinksList, mediaItems, updateMediaItems));
+  }, [mediaItems]);
 
   return (
-    <div className="media-links-container columns is-gapless">{mediaLinkItems}</div>
+    <div className="media-links-container columns is-gapless">{mediaLinkItems.map(
+      (item: MediaLinkItemProps, index: number): JSX.Element => <MediaLinkItem key={index} {...item}/>
+    )}</div>
   )
 }
 
